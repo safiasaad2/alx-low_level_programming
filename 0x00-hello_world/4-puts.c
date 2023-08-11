27a4687085a72f1abe17a5fef8f09f06976c135c
safia <you@example.com>
2023-08-10 18:11:20 -0700

    task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main function
+ *
+ * Description: c program for printing a statement with put
+ *
+ * return:always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
