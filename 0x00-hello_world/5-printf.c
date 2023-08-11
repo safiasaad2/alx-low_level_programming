ac8f334f34e8ad320ce2429dfcf4be9900809de8
safia <you@example.com>
2023-08-10 18:37:57 -0700

    task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,9 +6,9 @@
  * Description:c program for brinting statement with printf function
  *
  * Return:always 0
- */
+*/
 
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
