2c38e07caf1778b515a1b38ca9b950cb6524517d
safia <you@example.com>
2023-08-11 20:13:50 -0700

    task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Description: printing z-a
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char l = 'z';
+
+	while (l >= 'a')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+
+	return (0);
+}
