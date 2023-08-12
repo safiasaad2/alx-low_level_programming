14c4601d57fef805b4c7db046330139e7742a461
safia <you@example.com>
2023-08-11 19:38:44 -0700

    task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Description: printing aplhabet letters
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char l = 'a';
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+
+	return (0);
+}
