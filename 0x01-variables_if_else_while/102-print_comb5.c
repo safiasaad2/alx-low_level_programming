ea3868c1f08b88919cdf3c69eefd662e13c55846
safia <you@example.com>
2023-08-11 21:26:53 -0700

    task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Description: printing combinations of two digits
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int d1 = 0, d2;
+
+	while (d1 <= 99)
+	{
+		d2 = d1;
+		while (d2 <= 99)
+		{
+			if (d2 != d1)
+			{
+				putchar((d1 / 10) + 48);
+				putchar((d1 % 10) + 48);
+				putchar(' ');
+				putchar((d2 / 10) + 48);
+				putchar((d2 % 10) + 48);
+
+				if (d1 != 98 || d2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			d2++;
+		}
+		d1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
