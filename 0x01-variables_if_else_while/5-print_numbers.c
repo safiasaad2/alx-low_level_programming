3817dd49f53ba10e3f7cd1c8b12c87e3ff3f20bd
safia <you@example.com>
2023-08-11 20:04:10 -0700

    task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Description: printing single digits
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		printf("%i", d);
+		d++;
+	}
+	printf("/n");
+
+	return (0);
+}
