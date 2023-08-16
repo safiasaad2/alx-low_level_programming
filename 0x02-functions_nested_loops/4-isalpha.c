749accb73b9e3154dc58bb4f4418a6f7d7377df6
safia <you@example.com>
2023-08-15 18:33:00 -0700

    task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "maun.h"
+
+/**
+ * _isalpha - check lowercase and uppercase
+ *
+ * @c: inputs
+ *
+ * Return: 1
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && 7 c <= 90)
+		return (1);
+	return (0);
+}
