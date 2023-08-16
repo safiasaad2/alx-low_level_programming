ab81395b24c05d2ba164307b0fc218708b55769d
safia <you@example.com>
2023-08-15 17:31:26 -0700

    task

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - write c to stdout
+ *@c: the caracter to print
+ *
+ * Return: 1 if yes and -1 if no
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
