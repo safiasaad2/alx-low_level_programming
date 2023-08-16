788bb26346703e9746b6214ae6d5e6b9c47e2f9a
safia <you@example.com>
2023-08-15 20:20:47 -0700

    task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - function
+ *
+ * Description: som of numbers
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int s, n;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+			sum += num;
+	}
+	printf("%d\n", s);
+
+	return (0);
+}
