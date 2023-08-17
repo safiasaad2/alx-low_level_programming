e47833448d15b6d4df341653d475c350011ac369
safia <you@example.com>
2023-08-17 14:52:03 -0700

    task

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,29 @@
+/**
+* largest_number - prints the largest of 3 integers
+*
+* @a: input
+* @b: input
+* @c: input
+*
+* Return: 0
+*/
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (0);
+}
