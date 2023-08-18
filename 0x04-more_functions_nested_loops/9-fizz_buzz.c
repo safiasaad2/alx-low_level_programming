e6e6c840694173749781f1942ed78306f3a1161c
safia <you@example.com>
2023-08-17 18:24:13 -0700

    task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && !(n % 5 == 0))
+			printf("Fizz");
+		else if (n % 5 == 0 && !(n % 3 == 0))
+			printf("Buzz");
+		else if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", num);
+		if (n != 100)
+			printf(" ");
+		else
+			printf('\n');
+	}
+
+	return (0);
+}
