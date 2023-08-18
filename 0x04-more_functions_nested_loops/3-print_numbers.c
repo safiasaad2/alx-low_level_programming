cf98d467edf3f7008c4d17e92157bdf35c61986f
safia <you@example.com>
2023-08-17 17:08:01 -0700

    task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function
+ *
+ * Return :0
+*/
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	do {
+		_putchar(n + 48);
+		n++;
+	} while (n >= 0 && n <= 9);
+	_putchar('\n');
+}
