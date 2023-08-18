8e6f18fcc5802d365de9b607fb54d23f44c3bf3a
safia <you@example.com>
2023-08-17 17:23:24 -0700

    task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int n, r, c;
+
+	for (r = 1; r <= 10; r++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			n = c;
+			if (c > 9)
+			{
+				_putchar(1 + 48);
+				n = c % 10;
+			}
+			_putchar(n + 48);
+		}
+		_putchar('\n');
+	}
+}
