07a935b9c30b2aed289dcfd2eccdab5606f6f7b5
safia <you@example.com>
2023-08-21 20:00:38 -0700

    task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - function
+ * Return: 0
+*/
+
+int main(void)
+{
+	int s;
+	char f;
+
+	srand(time(NULL));
+	while (s <= 
