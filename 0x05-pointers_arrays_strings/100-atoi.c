086c0f0d4c9fde4b73e3f4a2ea28bdc09a3cd52b
safia <you@example.com>
2023-08-21 19:38:06 -0700

    task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,16 +8,16 @@
 int _atoi(char *s)
 {
 	unsigned int n = 0;
-	int s = 1;
+	int S = 1;
 
 	do {
 		if (*s == '-')
-			s *= -1;
+			S *= -1;
 		else if (*s >= '0' && *s <= '9')
 			n = (n * 10) + (*s - '0');
 		else if (n > 0)
 			break;
 	} while (*s++);
 
-	return (n * s);
+	return (n * S);
 }
