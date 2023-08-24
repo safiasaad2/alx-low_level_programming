dc91ad53cc888093afb209414189baad0fdcb146
safia <you@example.com>
2023-08-24 16:09:24 -0700

    task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strncat - function
+ *
+ * @dest: input
+ * @src: input
+ *
+ * Return: the pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int s, s1;
+
+	s = 0;
+	while (dest[s])
+		s++;
+
+	for (s1 = 0; src[s1]; s1++)
+		dest[s++] = src[s1];
+
+	return (dest);
+}
