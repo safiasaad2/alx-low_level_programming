5aede44d5e127ef2e1f1fba3acfb42c20be71802
safia <you@example.com>
2023-08-28 17:57:05 -0700

    task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ **_memcpy - function
+ *
+ * @dest: input
+ * @src: input
+ * @n: input
+ *
+ * Return :the pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int r;
+
+	for (r = 0; r < n; r++)
+	{
+		dedt[r] = src[r];
+	}
+	return (dest);
+}
