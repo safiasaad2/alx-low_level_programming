b6f5552e47ce088e53bb4c983cf17266e59a34f2
safia <you@example.com>
2023-08-28 18:45:43 -0700

    task

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function
+ *
+ * @a: input
+ * Return: nothing
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int r, R;
+
+	for (r = 0; r < 8; r++)
+	{
+		for (R = 0; R < 8; R++)
+		{
+			putchar(a[r][R]);
+		}
+		_putchar('\n');
+	}
+}
